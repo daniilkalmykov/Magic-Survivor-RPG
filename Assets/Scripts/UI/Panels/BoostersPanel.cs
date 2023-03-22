03777f98cb12846c1bfda136995c814f0fa16523
daniilkalmykov <daniilk24960@gmail.com>
2023-03-22 22:20:48 +0600

    fix BoostersPanel.cs

--- Assets/Scripts/UI/Panels/BoostersPanel.cs
+++ Assets/Scripts/UI/Panels/BoostersPanel.cs
@@ -1,3 +1,4 @@
+using UI.Views;
 using UnityEngine;
 
 namespace UI.Panels
@@ -5,17 +6,22 @@ namespace UI.Panels
     public sealed class BoostersPanel : MonoBehaviour
     {
         [SerializeField] private GameLogic.Education _education;
+        [SerializeField] private TimerView _timerView;
         
         private void OnEnable()
         {
             Time.timeScale = 0;
+            
             _education.gameObject.SetActive(false);
+            _timerView.gameObject.SetActive(false);
         }
 
         private void OnDisable()
         {
             Time.timeScale = 1;
+            
             _education.gameObject.SetActive(true);
+            _timerView.gameObject.SetActive(true);
         }
     }
 }
