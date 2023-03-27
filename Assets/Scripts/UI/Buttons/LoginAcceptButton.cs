bd4e8ec6e46b86d69513a6f320e883a31d646043
daniilkalmykov <daniilk24960@gmail.com>
2023-03-27 22:29:07 +0600

    fix rejects form yandex

---
+++ Assets/Scripts/UI/Buttons/LoginAcceptButton.cs
@@ -0,0 +1,32 @@
+using Agava.YandexGames;
+using UI.Views;
+using UnityEngine;
+using Leaderboard = YandexSDK.Leaderboard;
+
+namespace UI.Buttons
+{
+    public sealed class LoginAcceptButton : GameButton
+    {
+        [SerializeField] private Leaderboard _leaderboard;
+        [SerializeField] private LeaderboardView _leaderboardView;
+        
+        protected override void OnClick()
+        {
+            OpenLeaderboard();
+        }
+
+        private void OpenLeaderboard()
+        {
+            PlayerAccount.Authorize();
+
+            if (PlayerAccount.IsAuthorized)
+                PlayerAccount.RequestPersonalProfileDataPermission();
+            
+            if(PlayerAccount.IsAuthorized == false)
+                return;
+            
+            _leaderboardView.gameObject.SetActive(true);
+            _leaderboard.Fill();
+        }
+    }
+}
